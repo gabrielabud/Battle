44e47432edb086f09a0206ff2962c5410b36ff93
antonio <antoniobelmardacosta@gmail.com>
2017-11-08 16:23:39 +0000

    Adds post-redirect loop

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
-  post '/players' do
-    @pl1 = params[:player1]
-    @pl2 = params[:player2]
+  get "/play" do
+    @pl1 = session[:pl1]
+    @pl2 = session[:pl2]
     erb(:play)
   end
+
+  post '/names' do
+    session[:pl1] = params[:player1]
+    session[:pl2] = params[:player2]
+    redirect "/play"
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
