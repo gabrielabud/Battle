81aa137acc3bf646521ed7f6e3d6a9e3e00984e0
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-10 14:31:46 +0000

    Losing and Winning

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,5 +6,20 @@ feature 'attacking oppents' do
     click_button 'Attack'
     expect(page).to have_content 'Gabriela attacked Lewis'
   end
-  
+end
+
+feature 'announcing loser' do
+  scenario 'player1 loses' do
+    sign_in_and_play
+    18.times { attack_back }
+    click_button 'Attack'
+    expect(page).to have_content 'Lewis you lost'
+  end
+
+  scenario 'player2 loses' do
+    sign_in_and_play
+    19.times { attack_back }
+    click_button 'Attack'
+    expect(page).to have_content 'Gabriela you lost'
+  end
 end
